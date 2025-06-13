97a4af72003caa00b8f037529e9ff148eb5fe103
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-13 11:19:36 +0530

    all mathematical operations follow the rules of matrix operation

--- tensor.py
+++ tensor.py
@@ -3,21 +3,21 @@ import tensorflow as tf
 # tensor are like matrices
 
 #rank 1 tensor
-# x = tf.constant([1,2,3])
-# print(x)
+x = tf.constant([1,2,3])
+print(x)
 
 #rank 2 tensor
-# y = tf.constant([[1,2,3],[4,5,6]])
-# print(y)
-#
-# z = tf.ones([2,2])
-# print(z)
-#
-# w = tf.random.normal([2,2], mean=0, stddev=1)
-# print(w)
-#
-# x = tf.cast([1,2,3],tf.float32)
-# print(x)
+y = tf.constant([[1,2,3],[4,5,6]])
+print(y)
+
+z = tf.ones([2,2])
+print(z)
+
+w = tf.random.normal([2,2], mean=0, stddev=1)
+print(w)
+
+x = tf.cast([1,2,3],tf.float32)
+print(x)
 
 # all operation in tensor are elementwise
 
@@ -27,5 +27,11 @@ y = tf.constant([4,5,6])
 z = tf.add(x,y)
 print(z)
 
-z = tf.tensordot(x,y,[1,1])
+z = tf.tensordot(x,y,1)
 print(z)
+
+a = tf.random.normal((2, 2))
+b = tf.random.normal((2, 2))
+
+c = tf.matmul(a,b)
+print(c)
