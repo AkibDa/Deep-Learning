9d50457435a9024d690bfb5c2d0528a7ad09e9f8
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-13 15:12:05 +0530

    getting the data

--- regression.py
+++ regression.py
@@ -7,4 +7,10 @@ np.set_printoptions(precision=3, suppress=True)
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.layers.experimental import preprocessing
+
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data"
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight','Acceleration','Model Year','Origin']
+
+dataset = pd.read_csv(url, names=column_names, na_values='?', comment='\t', sep=' ', skipinitialspace=True)
+
+print(dataset.tail())
