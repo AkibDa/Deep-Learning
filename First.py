10f144184d5170c420fd8b91e77f920cc6889b3d
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-13 11:11:03 +0530

    understanding concept of tensors

--- First.py
+++ First.py
@@ -1,3 +1,12 @@
 import tensorflow as tf
 
-print(tf.__version__)
+#rank 1 tensor
+x = tf.constant([1,2,3])
+print(x)
+
+#rank 2 tensor
+y = tf.constant([[1,2,3],[4,5,6]])
+print(y)
+
+z = tf.ones([2,2])
+print(z)
