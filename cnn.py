11e4c97fe455621cad34d171395982ba81487af6
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-14 11:29:04 +0530

    loading , splitting the data into training and testing data

--- cnn.py
+++ cnn.py
@@ -2,3 +2,12 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import  layers
 import matplotlib.pyplot as plt
+
+cifar10 = keras.datasets.cifar10
+
+(train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+
+print(train_images.shape)
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
