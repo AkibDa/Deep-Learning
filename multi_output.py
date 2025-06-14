34047c545c30486b9ffb8f0138c311a916713671
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-14 15:23:44 +0530

    compiling the functional model

--- multi_output.py
+++ multi_output.py
@@ -15,3 +15,16 @@ outputs2 = dense3(x)
 
 model = tf.keras.Model(inputs=inputs, outputs=[outputs1, outputs2], name="mnist_model")
 model.summary()
+
+# loss and optimizer
+loss1 = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+loss2 = tf.keras.losses.BinaryCrossentropy(from_logits=False)
+optimizer = tf.keras.optimizers.Adam()
+metrics = ['accuracy']
+
+losses = {
+  'category_output': loss1,
+  'leftright_output': loss2,
+}
+
+model.compile(optimizer=optimizer, loss=losses, metrics=metrics)
