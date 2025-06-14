b172ba4cced8435180b2cdbe8ef67a20bee107c1
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-14 16:01:46 +0530

    splitting the data into training , testing and validating data

--- Classify_Lego_Star_Wars.py
+++ Classify_Lego_Star_Wars.py
@@ -12,3 +12,35 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+BASE_DIR = 'data/star-wars/'
+names = ['YODA', 'LUKE SKYWALKER', 'R2-D2', 'MACE WINDU', 'GENERAL GRIEVOUS']
+
+tf.random.set_seed(1)
+
+# Read information about dataset
+if not os.path.isdir(BASE_DIR + 'train/'):
+  for name in names:
+    os.makedirs(BASE_DIR + 'train/' + name)
+    os.makedirs(BASE_DIR + 'test/' + name)
+    os.makedirs(BASE_DIR + 'val/' + name)
+
+# Total number of classes in the dataset
+orig_folders = ['0001/', '0002/', '0003/', '0004/', '0005/']
+for folder_idx, folder in enumerate(orig_folders):
+  files = os.listdir(BASE_DIR + folder)
+  number_of_images = len([name for name in files])
+  n_train = int((number_of_images * 0.6) + 0.5)
+  n_valid = int((number_of_images * 0.25) + 0.5)
+  n_test = number_of_images - n_train - n_valid
+  print(number_of_images, n_train, n_valid, n_test)
+  for idx, file in enumerate(files):
+    file_name = BASE_DIR + folder +  file
+    if idx < n_train:
+      shutil.move(file_name, BASE_DIR + 'train/' + names[folder_idx])
+    elif idx < n_train + n_valid:
+      shutil.move(file_name, BASE_DIR + 'val/' + names[folder_idx])
+    else:
+      shutil.move(file_name, BASE_DIR + 'test/' + names[folder_idx])
+
+
+
