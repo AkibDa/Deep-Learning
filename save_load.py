c261ec7a62515842f39765fb0369061a97f9c9e9
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-14 11:47:54 +0530

    making the neural network

--- save_load.py
+++ save_load.py
@@ -5,3 +5,9 @@ import numpy as np
 mnist = keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = keras.models.Sequential([
+  keras.layers.Flatten(input_shape=(28,28)),
+  keras.layers.Dense(128, activation='relu'),
+  keras.layers.Dense(10),
+])
