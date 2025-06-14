39c96b35768dbb27d4161f8edcee36d4795dbc03
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-14 11:46:37 +0530

    loading and normalizing the data

--- save_load.py
+++ save_load.py
@@ -2,3 +2,6 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
+mnist = keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
