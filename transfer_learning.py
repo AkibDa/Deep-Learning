04fe3eee3ea180503f39c903724cfa7e35c301b0
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-15 10:10:13 +0530

    converting the functional model into sequential model

--- transfer_learning.py
+++ transfer_learning.py
@@ -3,3 +3,9 @@ import tensorflow as tf
 vgg_model = tf.keras.applications.VGG19()
 print(type(vgg_model))
 print(vgg_model.summary())
+
+model = tf.keras.models.Sequential()
+for layer in vgg_model.layers[0:-1]:
+  model.add(layer)
+
+print(model.summary())
