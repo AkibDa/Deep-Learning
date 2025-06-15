a1113dbbfd75c0db2079a3ee4c0267339b246628
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-15 10:31:47 +0530

    Learning about RNN

---
+++ rnn.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+mnist = keras.datasets.mnist
+
+# loading data
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+
+# normalising 0,255 --> 0,1
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# model
+model = keras.models.Sequential([
+  keras.layers.Flatten(input_shape=(28, 28)),
+  keras.layers.Dense(128, activation=tf.nn.relu),
+  keras.layers.Dense(10),
+])
+
+print(model.summary())
+# model = keras.Sequential()
+# model.add(keras.layers.Flatten(input_shape=(28, 28)))
+# model.add(keras.layers.Dense(128, activation=tf.nn.relu))
+# model.add(keras.layers.Dense(10))
+
+# loss and optimizer
+loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+optim = keras.optimizers.Adam()
+metrics = ['accuracy']
+
+model.compile(loss=loss, optimizer=optim, metrics=metrics)
+
+# training
+batch_size = 64
+epochs = 5
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, shuffle=True, verbose=1)
+
+# evaluate
+model.evaluate(x_test, y_test, batch_size=batch_size, verbose=1)
+
+# predictions
+probability_model = keras.models.Sequential([
+  model,
+  keras.layers.Softmax()
+])
+
+predictions = probability_model(x_test)
+print(predictions[0])
+label = np.argmax(predictions[0])
+print(label)
+
+# model + softmax
+predictions = model(x_test)
+predictions = tf.nn.softmax(predictions)
+pred0 = predictions[0]
+print(pred0)
+label0 = np.argmax(pred0)
+print(label0)
+
+pred05s = predictions[0:5]
+print(pred05s.shape)
+label05 = np.argmax(pred05s, axis=1)
+print(label05)
