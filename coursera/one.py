c6ff31c516d5d2093df65a9d4b5c7e91e76d5bc0
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-07-05 18:45:08 +0530

    Load and preprocess the dataset

--- coursera/one.py
+++ coursera/one.py
@@ -1,3 +1,9 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
+# Load the Fashion MNIST dataset
+(train_images, train_activityels), (test_images, test_activityels) = tf.keras.datasets.fashion_mnist.load_data()
+
+# Normalize the pixel values to be between 0 and 1
+train_images = train_images / 255.0
+test_images = test_images / 255.0
